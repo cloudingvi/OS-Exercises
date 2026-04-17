dfe04f8330ebfb23938d5f901231f49420a87353
cloudingvi <giorgia.conte@studenti.unitn.it>
2026-04-17 12:06:11 +0200

    fixed T3

--- exT3.c
+++ exT3.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void * function (int i) {
-    printf("thread n. %d reporting for duty!\n", i);
-    return;
+void * function (void * args) {
+    int *arr = (int *) args;
+    int i = arr[0];
+    int n = arr[1];
+    printf("thread n. %d out of %d reporting for duty!\n", i, n);
+    pthread_exit(NULL);
 }
-
+ 
 int main () {
     int numThreads;
     char * env = getenv("NUM_THREADS");
@@ -15,16 +18,17 @@ int main () {
         printf("no value found; defaulted to %d\n", numThreads);
     }
     else {
-        int n = atoi(numThreads);
-        printf("found value is %d\n", n);
+        numThreads = atoi(env);
+        printf("found value is %d\n", numThreads);
     }
 
     printf("got here with n = %d\n", numThreads);
 
     for (int i = 0; i < numThreads; i++) {
         pthread_t thread;
-        pthread_create(&thread, NULL, function, i);
-        pthread_join(&thread, NULL);
+        int args [2] = {i, numThreads};
+        pthread_create(&thread, NULL, function, (void *) args);
+        pthread_join(thread, NULL);
     }
 
     return 0;
